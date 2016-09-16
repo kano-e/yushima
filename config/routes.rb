7936bb3ae90a22cbe122e0b129722d340c2d0757
Eri Takano <e-takano@feedforce.jp>
2016-09-16 20:33:15 +0900

    $ annotate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# == Route Map
+#
+# You don't have any routes defined!
+#
+# Please add some routes in config/routes.rb.
+#
+# For more information about routes, see the Rails guide: http://guides.rubyonrails.org/routing.html.
+#
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
