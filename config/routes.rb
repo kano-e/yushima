bec741d52631c1f73aa07ec937e54521db072831
Eri Takano <e-takano@feedforce.jp>
2016-09-20 20:35:48 +0900

    annotate

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 # == Route Map
 #
-#    Prefix Verb   URI Pattern               Controller#Action
-#     games GET    /games(.:format)          games#index
-#           POST   /games(.:format)          games#create
-#  new_game GET    /games/new(.:format)      games#new
-# edit_game GET    /games/:id/edit(.:format) games#edit
-#      game GET    /games/:id(.:format)      games#show
-#           PATCH  /games/:id(.:format)      games#update
-#           PUT    /games/:id(.:format)      games#update
-#           DELETE /games/:id(.:format)      games#destroy
+#        Prefix Verb   URI Pattern                    Controller#Action
+#    activities GET    /activities(.:format)          activities#index
+#               POST   /activities(.:format)          activities#create
+#  new_activity GET    /activities/new(.:format)      activities#new
+# edit_activity GET    /activities/:id/edit(.:format) activities#edit
+#      activity GET    /activities/:id(.:format)      activities#show
+#               PATCH  /activities/:id(.:format)      activities#update
+#               PUT    /activities/:id(.:format)      activities#update
+#               DELETE /activities/:id(.:format)      activities#destroy
+#         games GET    /games(.:format)               games#index
+#               POST   /games(.:format)               games#create
+#      new_game GET    /games/new(.:format)           games#new
+#     edit_game GET    /games/:id/edit(.:format)      games#edit
+#          game GET    /games/:id(.:format)           games#show
+#               PATCH  /games/:id(.:format)           games#update
+#               PUT    /games/:id(.:format)           games#update
+#               DELETE /games/:id(.:format)           games#destroy
 #
 
 Rails.application.routes.draw do
-  resources :activities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :activities
   resources :games
 end
