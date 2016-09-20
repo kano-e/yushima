96038701684f9f6ab991222ec1da29fcfbd61c88
Eri Takano <e-takano@feedforce.jp>
2016-09-20 21:11:07 +0900

    $ annotate

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,31 @@
 # == Route Map
 #
-#        Prefix Verb   URI Pattern                    Controller#Action
-#    activities GET    /activities(.:format)          activities#index
-#               POST   /activities(.:format)          activities#create
-#  new_activity GET    /activities/new(.:format)      activities#new
-# edit_activity GET    /activities/:id/edit(.:format) activities#edit
-#      activity GET    /activities/:id(.:format)      activities#show
-#               PATCH  /activities/:id(.:format)      activities#update
-#               PUT    /activities/:id(.:format)      activities#update
-#               DELETE /activities/:id(.:format)      activities#destroy
-#         games GET    /games(.:format)               games#index
-#               POST   /games(.:format)               games#create
-#      new_game GET    /games/new(.:format)           games#new
-#     edit_game GET    /games/:id/edit(.:format)      games#edit
-#          game GET    /games/:id(.:format)           games#show
-#               PATCH  /games/:id(.:format)           games#update
-#               PUT    /games/:id(.:format)           games#update
-#               DELETE /games/:id(.:format)           games#destroy
+#                Prefix Verb   URI Pattern                           Controller#Action
+#     activity_comments GET    /activity_comments(.:format)          activity_comments#index
+#                       POST   /activity_comments(.:format)          activity_comments#create
+#  new_activity_comment GET    /activity_comments/new(.:format)      activity_comments#new
+# edit_activity_comment GET    /activity_comments/:id/edit(.:format) activity_comments#edit
+#      activity_comment GET    /activity_comments/:id(.:format)      activity_comments#show
+#                       PATCH  /activity_comments/:id(.:format)      activity_comments#update
+#                       PUT    /activity_comments/:id(.:format)      activity_comments#update
+#                       DELETE /activity_comments/:id(.:format)      activity_comments#destroy
+#            activities GET    /activities(.:format)                 activities#index
+#                       POST   /activities(.:format)                 activities#create
+#          new_activity GET    /activities/new(.:format)             activities#new
+#         edit_activity GET    /activities/:id/edit(.:format)        activities#edit
+#              activity GET    /activities/:id(.:format)             activities#show
+#                       PATCH  /activities/:id(.:format)             activities#update
+#                       PUT    /activities/:id(.:format)             activities#update
+#                       DELETE /activities/:id(.:format)             activities#destroy
+#                 games GET    /games(.:format)                      games#index
+#                       POST   /games(.:format)                      games#create
+#              new_game GET    /games/new(.:format)                  games#new
+#             edit_game GET    /games/:id/edit(.:format)             games#edit
+#                  game GET    /games/:id(.:format)                  games#show
+#                       PATCH  /games/:id(.:format)                  games#update
+#                       PUT    /games/:id(.:format)                  games#update
+#                       DELETE /games/:id(.:format)                  games#destroy
+#                  root GET    /                                     home#index
 #
 
 Rails.application.routes.draw do
