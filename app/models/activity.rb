c1bb8e82e6212a4c9744968bfab6befc51803bb2
Eri Takano <e-takano@feedforce.jp>
2016-09-20 20:32:31 +0900

    Create table activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,21 @@
+# ## Schema Information
+#
+# Table name: `activities`
+#
+# ### Columns
+#
+# Name              | Type               | Attributes
+# ----------------- | ------------------ | ---------------------------
+# **`id`**          | `integer`          | `not null, primary key`
+# **`created_at`**  | `datetime`         | `not null`
+# **`updated_at`**  | `datetime`         | `not null`
+# **`day`**         | `date`             | `not null`
+#
+# ### Indexes
+#
+# * `index_activities_on_day` (_unique_):
+#     * **`day`**
+#
+
 class Activity < ApplicationRecord
 end
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,3 +1,22 @@
+# ## Schema Information
+#
+# Table name: `activities`
+#
+# ### Columns
+#
+# Name              | Type               | Attributes
+# ----------------- | ------------------ | ---------------------------
+# **`id`**          | `integer`          | `not null, primary key`
+# **`created_at`**  | `datetime`         | `not null`
+# **`updated_at`**  | `datetime`         | `not null`
+# **`day`**         | `date`             | `not null`
+#
+# ### Indexes
+#
+# * `index_activities_on_day` (_unique_):
+#     * **`day`**
+#
+
 require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
