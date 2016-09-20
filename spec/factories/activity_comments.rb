3eafbc97d8c3bd1177f22656b49f86fd48de879d
Eri Takano <e-takano@feedforce.jp>
2016-09-20 21:13:25 +0900

    Create table activity_comments

--- spec/factories/activity_comments.rb
+++ spec/factories/activity_comments.rb
@@ -1,3 +1,27 @@
+# ## Schema Information
+#
+# Table name: `activity_comments`
+#
+# ### Columns
+#
+# Name               | Type               | Attributes
+# ------------------ | ------------------ | ---------------------------
+# **`id`**           | `integer`          | `not null, primary key`
+# **`created_at`**   | `datetime`         | `not null`
+# **`updated_at`**   | `datetime`         | `not null`
+# **`activity_id`**  | `integer`          | `not null`
+# **`game_id`**      | `integer`          |
+# **`photo`**        | `string`           |
+# **`detail`**       | `string`           |
+#
+# ### Indexes
+#
+# * `index_activity_comments_on_activity_id`:
+#     * **`activity_id`**
+# * `index_activity_comments_on_game_id`:
+#     * **`game_id`**
+#
+
 FactoryGirl.define do
   factory :activity_comment do
     activity nil
--- spec/models/activity_comment_spec.rb
+++ spec/models/activity_comment_spec.rb
@@ -1,3 +1,27 @@
+# ## Schema Information
+#
+# Table name: `activity_comments`
+#
+# ### Columns
+#
+# Name               | Type               | Attributes
+# ------------------ | ------------------ | ---------------------------
+# **`id`**           | `integer`          | `not null, primary key`
+# **`created_at`**   | `datetime`         | `not null`
+# **`updated_at`**   | `datetime`         | `not null`
+# **`activity_id`**  | `integer`          | `not null`
+# **`game_id`**      | `integer`          |
+# **`photo`**        | `string`           |
+# **`detail`**       | `string`           |
+#
+# ### Indexes
+#
+# * `index_activity_comments_on_activity_id`:
+#     * **`activity_id`**
+# * `index_activity_comments_on_game_id`:
+#     * **`game_id`**
+#
+
 require 'rails_helper'
 
 RSpec.describe ActivityComment, type: :model do
