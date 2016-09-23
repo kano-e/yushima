d8f46c5e3a67e9aa09b982f7bf5c6ea74330e727
Eri Takano <e-takano@feedforce.jp>
2016-09-23 21:40:21 +0900

    Use basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
 end
