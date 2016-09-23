004fb2a0d58ddd7398dfeb5b6f1ea0748341f9aa
Eri Takano <e-takano@feedforce.jp>
2016-09-23 21:19:37 +0900

    Fix PhotoUploader
    - Cloudinary にアップロードする
    - production 以外の環境では、ファイル名に環境名を入れる
    - タグにも環境名を入れる
    - create 前だと id が使えないのでファイル名用のランダムな文字列を用意する

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -7,14 +7,14 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -48,4 +48,21 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
+  include Cloudinary::CarrierWave
+
+  process convert: 'jpg'
+  process tags: ['yushima', 'photo', Rails.env]
+
+  version :standard do
+    process resize_to_fit: [1200, 1200]
+  end
+
+  version :thumbnail do
+    process resize_to_fill: [400, 400]
+  end
+
+  def public_id
+    base = Rails.env.production? ? '' : "#{Rails.env}-"
+    "#{base}#{model.class.name.underscore.gsub('_', '-')}-#{model.public_str}"
+  end
 end
