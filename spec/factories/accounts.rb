83cb3f23d78cf8bb2ef36bfedf6734d8defddc31
Eri Takano <e-takano@feedforce.jp>
2016-10-03 22:40:09 +0900

    Add model User and Account

---
+++ spec/factories/accounts.rb
@@ -0,0 +1,36 @@
+# ## Schema Information
+#
+# Table name: `accounts`
+#
+# ### Columns
+#
+# Name               | Type               | Attributes
+# ------------------ | ------------------ | ---------------------------
+# **`id`**           | `integer`          | `not null, primary key`
+# **`created_at`**   | `datetime`         | `not null`
+# **`updated_at`**   | `datetime`         | `not null`
+# **`provider`**     | `string`           | `not null`
+# **`uid`**          | `string`           | `not null`
+# **`user_id`**      | `integer`          |
+# **`credentials`**  | `json`             |
+# **`info`**         | `json`             |
+#
+# ### Indexes
+#
+# * `index_accounts_on_provider_and_uid` (_unique_):
+#     * **`provider`**
+#     * **`uid`**
+# * `index_accounts_on_user_id`:
+#     * **`user_id`**
+#
+# ### Foreign Keys
+#
+# * `accounts_belongs_to_users`:
+#     * **`user_id => users.id`**
+#
+
+FactoryGirl.define do
+  factory :account do
+    
+  end
+end
