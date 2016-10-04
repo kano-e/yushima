2ccd9fd8da5098c5a33091609b5955d3f37eaea9
Eri Takano <e-takano@feedforce.jp>
2016-10-04 22:23:09 +0900

    今時点では不必要なメニューだった

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/auth/:providr/callback', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
 
   resources :activities do
     resources :activity_comments
