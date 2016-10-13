9690ba78a6e8f9e5004dfdbf6c12a8eb796781cc
Eri Takano <e-takano@feedforce.jp>
2016-10-13 21:42:07 +0900

    めたタグ用のヘルパを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def og_tag(tag, content)
+    meta_tag("og:#{tag}", content)
+  end
+
+  def meta_tag(property, content)
+    tag(:meta, property: property, content: content)
+  end
+
   def signed_in?
     !current_user.nil?
   end
