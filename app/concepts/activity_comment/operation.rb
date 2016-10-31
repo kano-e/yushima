31dbdba8465ffc69362f1053f250fb416df545d7
Eri Takano <e-takano@feedforce.jp>
2016-10-31 21:24:57 +0900

    ActivityComment::Policy を導入

--- app/concepts/activity_comment/operation.rb
+++ app/concepts/activity_comment/operation.rb
@@ -1,7 +1,9 @@
 class ActivityComment::OperationBase < Trailblazer::Operation
   include Model
+  include Trailblazer::Operation::Policy
 
   model ActivityComment, :create
+  policy ActivityComment::Policy, :show?
 
   contract do
     property :game_id
@@ -17,12 +19,10 @@ class ActivityComment::OperationBase < Trailblazer::Operation
     [Game.pluck(:id, :title_ja), :first, :last]
   end
 
-  def process(params)
-    validate(params[:activity_comment]) do |form|
-      model.user = params[:current_user].presence
-      model.public_str ||= SecureRandom.urlsafe_base64
-      form.save
-    end
+  def form_save(form, params)
+    model.user = params[:current_user].presence
+    model.public_str ||= SecureRandom.urlsafe_base64
+    form.save
   end
 end
 
@@ -35,6 +35,14 @@ class ActivityComment::Create < ActivityComment::OperationBase
       Activity.find(params[:activity_id]).activity_comments.build
     end
   end
+
+  def process(params)
+    return invalid! unless policy.create?
+
+    validate(params[:activity_comment]) do |form|
+      form_save(form, params)
+    end
+  end
 end
 
 class ActivityComment::Update < ActivityComment::OperationBase
@@ -45,6 +53,14 @@ class ActivityComment::Update < ActivityComment::OperationBase
       ActivityComment.where(activity_id: params[:activity_id]).includes(:activity, :game).find(params[:id])
     else
       ActivityComment.all.includes(:activity, :game).find(params[:id])
+    end
+  end
+
+  def process(params)
+    return invalid! unless policy.update?
+
+    validate(params[:activity_comment]) do |form|
+      form_save(form, params)
     end
   end
 end
