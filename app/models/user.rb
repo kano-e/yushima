8a35736fa8d82a3905a89cae27cd2f96cf942636
Eri Takano <e-takano@feedforce.jp>
2016-10-31 19:45:13 +0900

    Add column role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@
 # **`created_at`**  | `datetime`         | `not null`
 # **`updated_at`**  | `datetime`         | `not null`
 # **`image_url`**   | `text`             |
+# **`role`**        | `integer`          | `default(0), not null`
 #
 
 class User < ApplicationRecord
