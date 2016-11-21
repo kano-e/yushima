a1893427afd708b64b522ec363e66ea22777cc3e
Eri Takano <e-takano@feedforce.jp>
2016-11-21 15:27:30 +0900

    一覧を追加
    `#index` アクション相当

--- app/graph/yushima_schema/query.rb
+++ app/graph/yushima_schema/query.rb
@@ -3,6 +3,12 @@ module YushimaSchema::Query
     name 'Query'
     description 'The query root of this schema'
 
+    field :games do
+      type Game::Graph::Type.to_list_type
+      description 'Game list'
+      resolve ->(obj, args, ctx) { Game.order(title_en: :asc, title_ja: :asc).to_a }
+    end
+
     field :game do
       type Game::Graph::Type
       argument :id, !types.ID
