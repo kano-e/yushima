69365fd6e6df66b011a3ca6640cc8689227baa35
Eri Takano <e-takano@feedforce.jp>
2016-11-24 22:20:40 +0900

    ソーシャルPLUS ID をユニークにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,12 @@
 # **`updated_at`**      | `datetime`         | `not null`
 # **`image_url`**       | `text`             |
 # **`role`**            | `integer`          | `default("user"), not null`
-# **`socialplus_uid`**  | `string`           |
+# **`socialplus_uid`**  | `string`           | `not null`
+#
+# ### Indexes
+#
+# * `index_users_on_socialplus_uid`:
+#     * **`socialplus_uid`**
 #
 
 class User < ApplicationRecord
