2a908326490940052b375d182743e6ee03f73985
Eri Takano <e-takano@feedforce.jp>
2016-12-01 17:07:49 +0900

    validation についてのテストも追加した

--- spec/concepts/game/operation_spec.rb
+++ spec/concepts/game/operation_spec.rb
@@ -32,4 +32,32 @@ describe 'operation for game' do
       end
     end
   end
+
+  describe 'validationについて' do
+    let(:user) { User.create(role: :admin, socialplus_uid: '12345') }
+
+    it 'title_jaは必須' do
+      expect do
+        Game::Create.(current_user: user, game: { title_ja: '' })
+      end.to raise_error(Trailblazer::Operation::InvalidContract)
+    end
+
+    it 'min_playersは数値のみ受け付ける' do
+      expect do
+        Game::Create.(current_user: user, game: { title_ja: 'テスト', min_players: 'two' })
+      end.to raise_error(Trailblazer::Operation::InvalidContract)
+    end
+
+    it 'min_playersは1以上' do
+      expect do
+        Game::Create.(current_user: user, game: { title_ja: 'テスト', min_players: '0' })
+      end.to raise_error(Trailblazer::Operation::InvalidContract)
+    end
+
+    it 'min_playersは99以下' do
+      expect do
+        Game::Create.(current_user: user, game: { title_ja: 'テスト', min_players: '100' })
+      end.to raise_error(Trailblazer::Operation::InvalidContract)
+    end
+  end
 end
